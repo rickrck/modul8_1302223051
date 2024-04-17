169166234f7c4fcf1384dedc354f0282555e268d
Ricky Renaldi <rickyrenaldi100@gmail.com>
2024-04-17 08:11:41 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302223051/BankTransferConfig.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul8_1302223051
+{
+    public class Config
+    {
+        public string lang { get; set; }
+
+        public class Transfer
+        {
+            [JsonPropertyName("threshold")]
+            public int threshold { get; set; }
+            [JsonPropertyName("low_fee")]
+            public int low_fee { get; set; }
+            [JsonPropertyName("high_fee")]
+            public int high_fee { get; set; }
+        }
+
+        public Transfer transfer { get; set; }
+
+        public string[] methods { get; set; }
+
+        public class Confirmation
+        {
+            [JsonPropertyName("en")]
+            public string en { get; set; }
+            [JsonPropertyName("id")]
+            public string id { get; set; }
+        }
+        public Confirmation confirmation { get; set; }
+    }
+    public class BankTransferConfig
+    {
+        public Config config { get; set; }
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string configFileName = "bank_transfer_config.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                setDefault();
+            }
+            catch
+            {
+                setDefault();
+                writeConfig();
+            }
+        }
+        public static Config Read()
+        {
+            string json = File.ReadAllText("bank_transfer_config.json");
+            return JsonSerializer.Deserialize<Config>(json);
+        }
+
+        private void writeConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(config, options);
+            string fullPath = path + '/' + configFileName;
+            File.WriteAllText(fullPath, jsonString);
+        }
+
+        public void setDefault()
+        {
+            config = new Config
+            {
+                lang = "en",
+                transfer = new Config.Transfer
+                {
+                    threshold = 25000000,
+                    low_fee = 6500,
+                    high_fee = 15000
+                },
+                methods = new string[] { "RTO (real-time)", "SKN", "RTGS", "BI FAST" },
+                confirmation = new Config.Confirmation
+                {
+                    en = "yes",
+                    id = "ya"
+                }
+
+            };
+        }
+
+
+
+
+
+    }
+}
--- modul8_1302223051/Program.cs
+++ modul8_1302223051/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using modul8_1302223051;
+
+internal class Program
+{
+    private static void Main()
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        int total = 0;
+        if (config.config.lang == "en")
+        {
+            Console.Write("Please insert the amount of money to transfer: ");
+        } else if (config.config.lang == "id")
+        {
+            Console.Write("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        int nominal_transfer = int.Parse(Console.ReadLine());
+
+        if (nominal_transfer <= config.config.transfer.threshold)
+        {
+            if (config.config.lang == "en")
+            {
+                Console.WriteLine("Transfer fee : " + config.config.transfer.low_fee);
+                Console.WriteLine("Total amount : " + (nominal_transfer + config.config.transfer.low_fee));
+            } else if (config.config.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer : " + config.config.transfer.low_fee);
+                Console.WriteLine("Total: " + (nominal_transfer + config.config.transfer.low_fee));
+            }
+        }
+        else
+        {
+            if (config.config.lang == "en")
+            {
+                Console.WriteLine("Transfer fee : " + config.config.transfer.high_fee);
+                Console.WriteLine("Total amount : " + (nominal_transfer + config.config.transfer.high_fee));
+            }
+            else if (config.config.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer : " + config.config.transfer.high_fee);
+                Console.WriteLine("Total: " + (nominal_transfer + config.config.transfer.high_fee));
+            }
+        }
+
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine("Select transfer method : ");
+        }
+        else if (config.config.lang == "id")
+        {
+            Console.WriteLine("Pilih metode transfer : ");
+        }
+
+        for (int i = 0; i < config.config.methods.Length; i++)
+        {
+            Console.WriteLine(i + 1 + ". " + config.config.methods[i]);
+        }
+
+        Console.ReadLine();
+
+        String confirm;
+        if (config.config.lang == "en")
+        {
+            Console.WriteLine("Please type " + config.config.confirmation.en + " to confirm the transaction: ");
+        }
+        else if (config.config.lang == "id")
+        {
+            Console.WriteLine("Ketik " + config.config.confirmation.en + " untuk mengkonfirmasi transaksi: ");
+        }
+
+        confirm = Console.ReadLine();
+
+        if (config.config.lang == "en")
+        {
+            if (confirm == config.config.confirmation.en)
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+        }
+        else if (config.config.lang == "id")
+        {
+            if (confirm == config.config.confirmation.id)
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+        }
+    }
+}
